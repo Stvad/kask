14938e17512c6aaada4a1d4beb5a3562a870a4d4
Vladyslav Sitalo <sitalov@amazon.com>
2018-08-18 22:41:40 +0100

    Delegate confirmationStatus to askIntent

--- src/main/kotlin/org/stvad/kask/model/intent.kt
+++ src/main/kotlin/org/stvad/kask/model/intent.kt
@@ -3,7 +3,7 @@ package org.stvad.kask.model
 import com.amazon.ask.model.IntentConfirmationStatus
 
 abstract class Intent(val askIntent: com.amazon.ask.model.Intent) {
-    val confirmationStatus: IntentConfirmationStatus = askIntent.confirmationStatus
+    val confirmationStatus: IntentConfirmationStatus get() = askIntent.confirmationStatus
 }
 
 abstract class BuiltInIntent(askIntent: com.amazon.ask.model.Intent) : Intent(askIntent)
