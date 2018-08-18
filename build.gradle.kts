36a1a9781296cac02a7f0c6a0d3a298ae35a1ff2
Vladyslav Sitalo <sitalov@amazon.com>
2018-08-18 21:42:24 +0100

    Move the gradle plugin into a separate project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,6 @@ plugins {
 
     kotlin("jvm") version "1.2.60"
     kotlin("kapt") version "1.2.60"
-
-    `java-gradle-plugin`
-    `maven-publish`
 }
 
 group = "org.stvad"
@@ -24,33 +21,14 @@ dependencies {
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.6.0")
     compile(kotlin("stdlib-jdk8"))
 
-    implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.2.60")
     compile("com.squareup", "kotlinpoet", "1.0.0-RC1")
     compile("com.amazon.alexa", "ask-sdk", "2.3.5")
-
-    testCompile("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-gradlePlugin {
-    plugins {
-        (plugins) {
-            "kask"{
-                id = "org.stvad.kask"
-                implementationClass = "org.stvad.kask.gradle.KaskGeneratorGradlePlugin"
-            }
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven(url = "build/repository")
-    }
 }
